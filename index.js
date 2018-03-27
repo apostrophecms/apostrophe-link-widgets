97441d6d8f07ea17c9a4e5cb9f9b66d236a782bb
Stuart Romanek <stuart@punkave.com>
2018-03-27 14:40:11 -0400

    link widgets schema should be an array to be leveraged in other schemas

--- index.js
+++ index.js
@@ -1,63 +1,9 @@
+var schema = require('./lib/schema.js');
+
 module.exports = {
   extend: 'apostrophe-widgets',
   label: 'Link',
   beforeConstruct: (self, options) => {
-    options.addFields = [
-      {
-        name: 'linkText',
-        label: 'Link Text',
-        type: 'string',
-        required: true
-      },
-      {
-        name: 'linkType',
-        label: 'Link Type',
-        type: 'select',
-        required: true,
-        choices: [
-          {
-            label: 'Page',
-            value: 'page',
-            showFields: [
-              '_linkPage'
-            ]
-          },
-          {
-            label: 'Custom',
-            value: 'custom',
-            showFields: [
-              'linkUrl'
-            ]
-          }
-        ]
-      },
-      {
-        name: '_linkPage',
-        label: 'Link Page',
-        type: 'joinByOne',
-        withType: 'apostrophe-page',
-        idField: 'pageId',
-        required: true,
-        filters: {
-          projection: {
-            title: 1,
-            slug: 1,
-            type: 1,
-            tags: 1
-          }
-        }
-      },
-      {
-        name: 'linkUrl',
-        label: 'Link URL',
-        type: 'url',
-        required: true
-      },
-      {
-        name: 'linkTarget',
-        label: 'Will the link open a new browser tab?',
-        type: 'boolean'
-      }
-    ].concat(options.addFields || []);
+    options.addFields = schema.concat(options.addFields || []);
   }
 };
